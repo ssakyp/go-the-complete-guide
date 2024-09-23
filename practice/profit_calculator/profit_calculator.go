ab77a4825afac4e36e40673f7db19e56b4f4a71f
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-23 10:12:23 +0500

    Update profit_calculator.go

--- practice/profit_calculator/profit_calculator.go
+++ practice/profit_calculator/profit_calculator.go
@@ -5,25 +5,39 @@ func main() {
   var expenses float64
   var taxRate float64
   
-  fmt.Print("Revenue: ")
-  fmt.Scan(&revenue)
-
+  // fmt.Print("Revenue: ")
+  // fmt.Scan(&revenue)
+  prompt("Revenue", &revenue)
   
-  fmt.Print("Expenses: ")
-  fmt.Scan(&expenses)
-
+  // fmt.Print("Expenses: ")
+  // fmt.Scan(&expenses)
+  prompt("Expenses", &expenses)
   
-  fmt.Print("Tax Rate: ")
-  fmt.Scan(&taxRate)
+  // fmt.Print("Tax Rate: ")
+  // fmt.Scan(&taxRate)
+  prompt("Tax Rate", &taxRate)
 
-  earningsBeforeTax := revenue - expenses
-  earningsAfterTax := earningsBeforeTax * (1 - taxRate / 100)
-  ratio := earningsBeforeTax / earningsAfterTax
-
+  // earningsBeforeTax := revenue - expenses
+  // earningsAfterTax := earningsBeforeTax * (1 - taxRate / 100)
+  // ratio := earningsBeforeTax / earningsAfterTax
+  
+  earningsBeforeTax, earningsAfterTax, ratio := profitCalculator(revenue, expenses, taxRate)
   fmt.Println(earningsBeforeTax)
   fmt.Println(earningsAfterTax)
   fmt.Println(ratio)
 
 
-  
+}
+
+func prompt(a string, b *float64) {
+  fmt.Print(a,": ")
+  fmt.Scan(b)
+}
+
+func profitCalculator(revenue, expenses, taxRate float64) (float64, float64, float64) {
+  ebf := revenue - expenses
+  profit := ebf * (1 - taxRate / 100)
+  ratio := ebf / profit
+
+  return ebf, profit, ratio
 }
