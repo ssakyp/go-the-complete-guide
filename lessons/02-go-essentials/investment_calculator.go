88d188f11f77be94ad5c3b54204d6ed91990b626
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-23 09:24:29 +0500

    Update investment_calculator.go

--- lessons/02-go-essentials/investment_calculator.go
+++ lessons/02-go-essentials/investment_calculator.go
@@ -11,9 +11,14 @@ import (
   "math"
   "fmt"
 )
+
 func main() {
+  const inflationRate = 2.5
   investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
   
   futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+  futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
+  
   fmt.Println(futureValue)
+  fmt.Println(futureRealValue)
 }
