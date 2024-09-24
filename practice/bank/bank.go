ce0d1970071d9f725dcb05f2d36e1d215e8bb445
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-24 09:13:44 +0500

    Update bank.go

--- practice/bank/bank.go
+++ practice/bank/bank.go
@@ -4,15 +4,26 @@ import (
   "fmt"
   "os"
   "strconv"
+  "errors"
 )
 
 const accountBalanceFile = "balance.txt"
 
-func getBalanceFromFile() float64 {
-  data, _ := os.ReadFile(accountBalanceFile)
+func getBalanceFromFile() (float64, error) {
+  data, err := os.ReadFile(accountBalanceFile)
+
+  if err != nil {
+    return 1000,  errors.New("Failed to find balance file.") 
+  }
+  
   balanceText := string(data)
-  balance, _ := strconv.ParseFloat(balanceText, 64)
-  return balance
+  balance, err := strconv.ParseFloat(balanceText, 64)
+
+  if err != nil {
+    return 1000, errors.New("Failed to parse stored balance value.")
+  }
+  
+  return balance, nil
 }
 
 func writeBalanceToFile(balance float64) {
@@ -21,7 +32,13 @@ func writeBalanceToFile(balance float64) {
 }
 
 func main() {
-  var accountBalance float64 = getBalanceFromFile()
+  var accountBalance, err = getBalanceFromFile()
+
+  if err != nil {
+    fmt.Println("ERROR")
+    fmt.Println(err)
+  }
+  
   fmt.Println("Welcome to Go Bank!")
 
   for {
