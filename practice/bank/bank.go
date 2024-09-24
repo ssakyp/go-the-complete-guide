8ce6126b21130e7bee3440395538bfb7992c65a4
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-24 09:15:22 +0500

    Update bank.go

--- practice/bank/bank.go
+++ practice/bank/bank.go
@@ -38,7 +38,7 @@ func main() {
     fmt.Println("ERROR")
     fmt.Println(err)
     fmt.Println("------------------------------")
-    return
+    // panic("Can't continue, sorry.")
   }
   
   fmt.Println("Welcome to Go Bank!")
