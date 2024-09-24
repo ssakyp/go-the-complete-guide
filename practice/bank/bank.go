5f6625d7c8779ff4ec3f0d457c5cab911a34bf92
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-24 08:46:02 +0500

    Update bank.go

--- practice/bank/bank.go
+++ practice/bank/bank.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+  var accountBalance float64 = 1000
+  
   fmt.Println("Welcome to Go Bank!")
   fmt.Println("What do you want to do?")
   fmt.Println("1. Check balance")
@@ -11,8 +13,14 @@ func main() {
   fmt.Println("4. Exit")
 
   var choice int
+  fmt.Print("Your choice: ")
   fmt.Scan(&choice)
 
+  //wantsCheckBalance := choice == 1
+  if choice == 1 {
+    fmt.Println("Your balance is: ", accountBalance)
+  }
+  
   fmt.Println("Your choice: ", choice)
 }
 
