27b5e4450ae6aef7a43f039cd1fc02376a18867c
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-25 09:04:40 +0500

    Update bank.go

--- practice/bank/bank.go
+++ practice/bank/bank.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "github.com/bank/fileops"
+  "github.com/Pallinder/go-randomdata"
 )
 
 const accountBalanceFile = "balance.txt"
@@ -20,6 +21,7 @@ func main() {
   }
   
   fmt.Println("Welcome to Go Bank!")
+  fmt.Println("Reach us 24/7 ", randomdata.PhoneNumber())
 
   for {
    presentOptions()
