3c21b874fc902d3e498dcb9c65aad9fdf3d07618
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-25 08:52:13 +0500

    Update bank.go

--- practice/bank/bank.go
+++ practice/bank/bank.go
@@ -9,27 +9,7 @@ import (
 
 const accountBalanceFile = "balance.txt"
 
-func getFloatFromFile(fileName string) (float64, error) {
-  data, err := os.ReadFile(fileName)
 
-  if err != nil {
-    return 1000,  errors.New("Failed to find file.") 
-  }
-  
-  valueText := string(data)
-  value, err := strconv.ParseFloat(valueText, 64)
-
-  if err != nil {
-    return 1000, errors.New("Failed to parse stored value.")
-  }
-  
-  return value, nil
-}
-
-func writeFloatToFile(value float64, fileName string) {
-  valueText := fmt.Sprint(balance)
-  os.WriteFile(fileName, []byte(valueText), 0644)
-}
 
 func main() {
   var accountBalance, err = getFloatFromFile(accountBalanceFile)
