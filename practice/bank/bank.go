bcff05429f8378097ad88bb84eec23c95a5d7fd7
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-25 08:52:02 +0500

    Update bank.go

--- practice/bank/bank.go
+++ practice/bank/bank.go
@@ -9,30 +9,30 @@ import (
 
 const accountBalanceFile = "balance.txt"
 
-func getBalanceFromFile() (float64, error) {
-  data, err := os.ReadFile(accountBalanceFile)
+func getFloatFromFile(fileName string) (float64, error) {
+  data, err := os.ReadFile(fileName)
 
   if err != nil {
-    return 1000,  errors.New("Failed to find balance file.") 
+    return 1000,  errors.New("Failed to find file.") 
   }
   
-  balanceText := string(data)
-  balance, err := strconv.ParseFloat(balanceText, 64)
+  valueText := string(data)
+  value, err := strconv.ParseFloat(valueText, 64)
 
   if err != nil {
-    return 1000, errors.New("Failed to parse stored balance value.")
+    return 1000, errors.New("Failed to parse stored value.")
   }
   
-  return balance, nil
+  return value, nil
 }
 
-func writeBalanceToFile(balance float64) {
-  balanceText := fmt.Sprint(balance)
-  os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
+func writeFloatToFile(value float64, fileName string) {
+  valueText := fmt.Sprint(balance)
+  os.WriteFile(fileName, []byte(valueText), 0644)
 }
 
 func main() {
-  var accountBalance, err = getBalanceFromFile()
+  var accountBalance, err = getFloatFromFile(accountBalanceFile)
 
   if err != nil {
     fmt.Println("ERROR")
@@ -66,7 +66,7 @@ func main() {
     // continue => skip the rest of the code
     accountBalance += depositAmount
     fmt.Println("Balance updated! New amount: ", accountBalance)
-    writeBalanceToFile(accountBalance)
+    writeFloatToFile(accountBalance, accountBalanceFile)
   }
   case 3: 
     var withdrawAmount float64
@@ -77,7 +77,7 @@ func main() {
     }
     accountBalance -= withdrawAmount
     fmt.Println("Balance updated! New amount: ", accountBalance)
-    writeBalanceToFile(accountBalance)
+    writeFloatToFile(accountBalance, accountBalanceFile)
   default:
     fmt.Println("Goodbye!")
     fmt.Println("Thanks for choosing our bank!")
