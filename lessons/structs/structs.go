461b170ebf462b311e30674f1e1d6b46374c7740
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-26 08:51:02 +0500

    Update structs.go

--- lessons/structs/structs.go
+++ lessons/structs/structs.go
@@ -8,6 +8,15 @@ func main() {
   birthDate "= getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
   // ... do something awesome with that gathered data
-      
+
+  fmt.Println(firstName, lastName, birthDate)
                           
+}
+
+
+func getUserData(promptText string) string {
+  fmt.Println(promptText)
+  var value string
+  fmt.Scan(&value)
+  return value
 }
