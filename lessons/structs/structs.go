0e92ae83080970e2a48ebd65ded598d1f1d6a256
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-26 08:55:03 +0500

    Update structs.go

--- lessons/structs/structs.go
+++ lessons/structs/structs.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "time"
+)
+type user struct {
+  firstName string
+  lastName string
+  birthDate string
+  createdAt time.Time
+}
 
 func main() {
   firstName := getUserData("Please enter your first name: ")
