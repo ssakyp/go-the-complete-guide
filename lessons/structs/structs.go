3c4a70e1e6790e039a37699dd43c264ecd9d6a4c
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-26 09:19:57 +0500

    Update structs.go

--- lessons/structs/structs.go
+++ lessons/structs/structs.go
@@ -6,35 +6,7 @@ import (
   "errors"
 )
 
-type user struct {
-  firstName string
-  lastName string
-  birthDate string
-  createdAt time.Time
-}
 
-func (u *user) outputUserDetails() {
-  // ...
-   fmt.Println((*u).firstName, u.lastName, u.birthDate)
-}
-
-func (u *user) clearUserName() {
-  u.firtsName = ""
-  u.lastName = ""
-}
-
-// constructor pattern
-func newUser(firstName, lastName, birthDate string) (*user, error) {
-  if firtsName == "" || lastName == "" || birthDate == "" {
-      return nil, errors.New("First name, last name, birth date is required!")
-  }
-  return &user {
-    firstName: firstName,
-    lastName: lastName,
-    birthDate: birthDate,
-    createdAt: time.Now(),
-  }, nil
-}
 func main() {
   userFirstName := getUserData("Please enter your first name: ")
   userLastName := getUserData("Please enter your first name: )
