de36791c521918dc64a93636f68bc634ef5ac978
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-26 10:19:50 +0500

    Update note.go

--- practice/note/note/note.go
+++ practice/note/note/note.go
@@ -1,6 +1,12 @@
 package note
 
-import "time"
+import (
+  "time"
+  "encoding/json"
+  "errors"
+  "fmt"
+  "os"
+)
 
 type Note struct {
   title string
@@ -8,8 +14,21 @@ type Note struct {
   createdAt time.Time
 }
 
+
 func (note Note) Display() {
   fmt.Printf("Your not titled %v has the following content:\n\n%v", note.title, note.content)
+}
+
+func (note Note) Save() error {
+  fileName := strings.ReplaceAll(note.title, " ", "_")
+  fileName := strings.ToLower(fileName)
+
+  json, err := json.Marshal(note)
+
+  if err != nil {
+    return err
+  }
+  return os.WriteFile(fileName, json, 0644)
 }
 
 func New(title, content string) (Note, error) {
