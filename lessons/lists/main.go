b28ace1199487eae6bb52455bc420ec77005b85e
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-27 10:42:36 +0500

    Update main.go

--- lessons/lists/main.go
+++ lessons/lists/main.go
@@ -1,10 +1,23 @@
 package main
 
 func main() {
-  userNames := make([]string, 2)
-
+  // if we know the number of elements in advance use make
+  userNames := make([]string, 2, 5)
+  //userNames := []string{}
+  
+  userNames[0] = "Julie"
   userNames = append(userNames, "Max")
   userNames = append(userNames, "Manuel")
 
   fmt.Println(userNames)
+
+  // for maps only len can be set
+  courseRatings := make(map[string]float64, 3)
+  //courseRatings := map[string]float64{}
+
+  courseRatings["go"] = 4.7
+  courseRatings["reac"] = 4.8
+  courseRatings["node"] = 4.7
+
+  fmt.Println(courseRatings)
 }
