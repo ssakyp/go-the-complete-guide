eba5231b12bd0d9375f2520f2c03b341589802b9
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-27 08:10:02 +0500

    Update todo.go

--- practice/note/todo/todo.go
+++ practice/note/todo/todo.go
@@ -1,1 +1,37 @@
+package todo
 
+import (
+  "time"
+  "encoding/json"
+  "errors"
+  "fmt"
+  "os"
+)
+
+type Todo struct {
+  Text string  `json:"text"`
+}
+
+
+func (todo Todo) Display() {
+  fmt.Println(todo.Text)
+}
+
+func (todo Todo) Save() error {
+  fileName :=  "todo.json"
+
+  json, err := json.Marshal(todo)
+
+  if err != nil {
+    return err
+  }
+  return os.WriteFile(fileName, json, 0644)
+}
+
+func New(content string) (Todo, error) {
+   if title == "" || content == "" {
+    return Todo{}, errors.New("Invalid input")
+  }
+
+  return Todo{Text: contennt}, nil
+}
