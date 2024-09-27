72bbeb7ee5c4a0fa8bc0f9d777c50f44e3f75e0f
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-27 08:49:13 +0500

    Update interface.go

--- lessons/interfaces/interface.go
+++ lessons/interfaces/interface.go
@@ -1,30 +1,42 @@
 package main
 
+import "fmt"
+
 func main() {
-  
+  result := add(1, 2)
+  fmt.Println(result)
 }
 
-func add(a, b interface{}) interface{} {
-  aInt, aIsInt := a.(int)
-  bInt, bIsInt := b.(int)
-
-  if aIsInt && bIsInt {
-    return aInt + bInt
+// generic type holder
+func add[T int|float64|string](a, b T) T {
+  return a + b
   }
 
-   aFloat, aIsFloat := a.(float64)
-  bFloat, bIsFloat := b.(float64)
 
-  if aIsFloat && bIsFloat {
-    return aFloat + bFloat
-  }
 
-   aStr, aIsStr := a.(string)
-  bStr, bIsStr := b.(string)
 
-  if aIsStr && bIsStr {
-    return aStr + bStr
-  }
+
+// func add(a, b interface{}) interface{} {
+//   aInt, aIsInt := a.(int)
+//   bInt, bIsInt := b.(int)
+
+//   if aIsInt && bIsInt {
+//     return aInt + bInt
+//   }
+
+//    aFloat, aIsFloat := a.(float64)
+//   bFloat, bIsFloat := b.(float64)
+
+//   if aIsFloat && bIsFloat {
+//     return aFloat + bFloat
+//   }
+
+//    aStr, aIsStr := a.(string)
+//   bStr, bIsStr := b.(string)
+
+//   if aIsStr && bIsStr {
+//     return aStr + bStr
+//   }
 
   
   
