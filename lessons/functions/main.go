542e30de9170d9d55bc46fe2e4f9659f8886119c
Kamilya Kydykova <sultansakyp77@gmail.com>
2024-09-28 19:23:24 +0500

    returning functions as values

--- lessons/functions/main.go
+++ lessons/functions/main.go
@@ -2,21 +2,43 @@ package main
 
 import "fmt"
 
+type tranformFn func(int) int
+
+//type anotherFn func(int, []string, map[string][]int) ([]int, string)
+
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2}
 	double := transformNumbers(&numbers, double)
 	tripple := transformNumbers(&double, triple)
 	fmt.Println(double)
 	fmt.Println(tripple)
+
+	transformerFn1 := getTransformerFunction(&numbers)
+	transformerFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers, transformerFn1)
+	moreTransformedNumbers := transformNumbers(&moreNumbers, transformerFn2)
+
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformedNumbers)
 }
 
-func transformNumbers(numbers *[]int, transform func(int) int) []int {
+func transformNumbers(numbers *[]int, transform tranformFn) []int {
 	dNumbers := []int{}
 	for _, v := range *numbers {
 		dNumbers = append(dNumbers, transform(v))
 	}
 
 	return dNumbers
+}
+
+func getTransformerFunction(numbers *[]int) tranformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
 
 func double(number int) int {
