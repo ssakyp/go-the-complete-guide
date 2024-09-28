2b6365e4a8a290eb28d149f8266add0cf77d7c7c
Kamilya Kydykova <sultansakyp77@gmail.com>
2024-09-28 19:37:25 +0500

    anonymous function

---
+++ lessons/functions/functionsarevalues/functionsarevalues.go
@@ -0,0 +1,50 @@
+package functionsarevalues
+
+import "fmt"
+
+type tranformFn func(int) int
+
+//type anotherFn func(int, []string, map[string][]int) ([]int, string)
+
+func main() {
+	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2}
+	double := transformNumbers(&numbers, double)
+	tripple := transformNumbers(&double, triple)
+	fmt.Println(double)
+	fmt.Println(tripple)
+
+	transformerFn1 := getTransformerFunction(&numbers)
+	transformerFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers, transformerFn1)
+	moreTransformedNumbers := transformNumbers(&moreNumbers, transformerFn2)
+
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformedNumbers)
+}
+
+func transformNumbers(numbers *[]int, transform tranformFn) []int {
+	dNumbers := []int{}
+	for _, v := range *numbers {
+		dNumbers = append(dNumbers, transform(v))
+	}
+
+	return dNumbers
+}
+
+func getTransformerFunction(numbers *[]int) tranformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
+}
---
+++ lessons/functions/go.mod
@@ -0,0 +1,3 @@
+module github.com/ssakyp/functions
+
+go 1.23.1
